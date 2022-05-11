3de478128ee1db6490ac05b9aaae8444c2fa42a3
jpalmat <jimmy.palma1990@gmail.com>
2022-05-10 22:15:27 -0500

    create product model

---
+++ src/app/shared/servicios/productos/model/producto-model.ts
@@ -0,0 +1,15 @@
+export class Product{
+ id_producto: string;
+ codigo: string;
+ nombre: string;
+ descripcion: string;
+ precio_compra: number;
+ precio_venta: number;
+ firma_farmaceutica: string;
+ concentracion: string;
+ registro_sanitario: string;
+ estado: boolean;
+ transaccion: string;
+ fecha_transaccion: string;
+ usuario: string;
+}
